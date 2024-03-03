717d51530e9f5cc939cdfd92e20c56fdfa30eeca
Ian Clester <ijc@ijc8.me>
2024-03-03 10:14:01 -0500

    typescript + vite

---
+++ editor/src/main.ts
@@ -0,0 +1,693 @@
+var $a = document.getElementById("audio");
+window.onkeydown = function(ev) {
+    if(ev.keyCode == 32) {
+        ev.preventDefault();
+        $a.pause();
+    }
+}
+
+var $trans = document.getElementById("transcript");
+var $preloader = document.getElementById('preloader');
+
+var wds = [];
+var cur_wd;
+
+var $phones = document.createElement("div");
+$phones.className = "phones";
+document.body.appendChild($phones);
+
+var cur_phones$ = [];           // List of phoneme $divs
+var $active_phone;
+
+function render_phones(wd) {
+    cur_phones$ = [];
+    $phones.innerHTML = "";
+    $active_phone = null;
+    
+    $phones.style.top = wd.$div.offsetTop + 15 + "px";
+    $phones.style.left = wd.$div.offsetLeft + "px";
+    
+    var dur = wd.end - wd.start;
+    
+    var start_x = wd.$div.offsetLeft;
+    
+    wd.phones
+    .forEach(function(ph){
+        var $p = document.createElement("span");
+        $p.className = "phone";
+        $p.textContent = ph.phone.split("_")[0];
+        
+        $phones.appendChild($p);
+        cur_phones$.push($p);
+    });
+    
+    var offsetToCenter = (wd.$div.offsetWidth - $phones.offsetWidth) / 2;
+    $phones.style.left = wd.$div.offsetLeft + offsetToCenter;
+}
+function highlight_phone(cur_wd, t) {
+    if(!cur_wd || !cur_wd.word) {
+        $phones.innerHTML = "";
+        return;
+    }
+    cur_wd = cur_wd.word
+    var hit;
+    var cur_t = cur_wd.start;
+    
+    cur_wd.phones.forEach(function(ph, idx) {
+        if(cur_t <= t && cur_t + ph.duration >= t) {
+            hit = idx;
+        }
+        cur_t += ph.duration;
+    });
+    
+    if(hit) {
+        var $ph = cur_phones$[hit];
+        if($ph != $active_phone) {
+            if($active_phone) {
+                $active_phone.classList.remove("phactive");
+            }
+            if($ph) {
+                $ph.classList.add("phactive");
+            }
+        }
+        $active_phone = $ph;
+    }
+}
+
+function highlight_word() {
+    var t = $a.currentTime;
+    // XXX: O(N); use binary search
+    var hits = blocks.filter(function(x) {
+        return (t - x.start) > 0.01 && (x.end - t) > 0.01;
+    });
+    var next_wd = hits[hits.length - 1];
+    
+    if(cur_wd != next_wd) {
+        console.log(next_wd?.text, next_wd)
+        var active = document.querySelectorAll('.active');
+        for(var i = 0; i < active.length; i++) {
+            active[i].classList.remove('active');
+        }
+        if (next_wd?.el) {
+            next_wd.el.classList.add('active');
+        }
+        if(next_wd?.word?.$div) {
+            render_phones(next_wd.word);
+        }
+    }
+    cur_wd = next_wd;
+    highlight_phone(cur_wd, t)
+    
+    window.requestAnimationFrame(highlight_word);
+}
+window.requestAnimationFrame(highlight_word);
+
+$trans.innerHTML = "Loading...";
+
+let blocks = []
+
+function render(ret) {
+    wds = ret['words'] || [];
+    transcript = ret['transcript'];
+    
+    $trans.innerHTML = '';
+    
+    var currentOffset = 0;
+    let currentTime = 0
+    
+    const addUnlinked = (nextOffset, nextTime) => {
+        var txt = transcript.slice(currentOffset, nextOffset);
+        let space = document.createElement("span")
+        space.className = "space"
+        var $plaintext = document.createTextNode(txt);
+        space.appendChild($plaintext)
+        let spaceStart = currentTime
+        space.onclick = () => {
+            $a.currentTime = spaceStart
+            $a.play()
+        };
+        $trans.appendChild(space);
+        currentOffset = nextOffset;
+        blocks.push({ text: txt, start: currentTime, end: nextTime, el: space })
+    }
+    
+    for (const wd of wds) {
+        if (wd.case == 'not-found-in-transcript') {
+            // TODO: show phonemes somewhere
+            var txt = ' ' + wd.word;
+            var $plaintext = document.createTextNode(txt);
+            $trans.appendChild($plaintext);
+            continue;
+        }
+        
+        // Add non-linked text
+        if (wd.startOffset > currentOffset) {
+            addUnlinked(wd.startOffset, wd.start)
+        }
+        
+        var $wd = document.createElement('span');
+        var txt = transcript.slice(wd.startOffset, wd.endOffset);
+        var $wdText = document.createTextNode(txt);
+        $wd.appendChild($wdText);
+        wd.$div = $wd;
+        if (wd.start !== undefined) {
+            $wd.className = 'success';
+        }
+        $wd.onclick = function() {
+            if(wd.start !== undefined) {
+                console.log(wd.start);
+                $a.currentTime = wd.start;
+                $a.play();
+            }
+        };
+        $trans.appendChild($wd);
+        currentOffset = wd.endOffset;
+        currentTime = wd.end
+        blocks.push({ text: txt, start: wd.start, end: wd.end, el: $wd, word: wd })
+    }
+    
+    addUnlinked(transcript.length, $a.duration)
+    console.log(blocks)
+}
+
+var status_init = false;
+var status_log  = [];		// [ status ]
+var $status_pro;
+
+function render_status(ret) {
+    if(!status_init) {
+        // Clobber the $trans div and use it for status updates
+        $trans.innerHTML = "<h2>transcription in progress</h2>";
+        $trans.className = "status";
+        $status_pro = document.createElement("progress");
+        $status_pro.setAttribute("min", "0");
+        $status_pro.setAttribute("max", "100");
+        $status_pro.value = 0;
+        $trans.appendChild($status_pro);
+        
+        status_init = true;
+    }
+    if(ret.status !== "TRANSCRIBING") {
+        if(ret.percent) {
+            $status_pro.value = (100*ret.percent);
+        }
+    }
+    else if(ret.percent && (status_log.length == 0 || status_log[status_log.length-1].percent+0.0001 < ret.percent)) {
+        // New entry
+        var $entry = document.createElement("div");
+        $entry.className = "entry";
+        $entry.textContent = ret.message;
+        ret.$div = $entry;
+        
+        if(ret.percent) {
+            $status_pro.value = (100*ret.percent);
+        }
+        
+        if(status_log.length > 0) {
+            $trans.insertBefore($entry, status_log[status_log.length-1].$div);
+        }
+        else {
+            $trans.appendChild($entry);
+        }
+        status_log.push(ret);
+    }
+}
+
+function update() {
+    if(INLINE_JSON) {
+        // We want this to work from file:/// domains, so we provide a
+        // mechanism for inlining the alignment data.
+        render(INLINE_JSON);
+    }
+    else  {
+        // Show the status
+        // get_json('status.json', function(ret) {
+        //     $a.style.visibility = 'hidden';
+        //     if (ret.status == 'ERROR') {
+        //         $preloader.style.visibility = 'hidden';
+        //         $trans.innerHTML = '<b>' + ret.status + ': ' + ret.error + '</b>';
+        //     } else if (ret.status == 'TRANSCRIBING' || ret.status == 'ALIGNING') {
+        //         $preloader.style.visibility = 'visible';
+        //         render_status(ret);
+        //         setTimeout(update, 2000);
+        //     } else if (ret.status == 'OK') {
+        //         $preloader.style.visibility = 'hidden';
+        //         // XXX: should we fetch the align.json?
+        //         window.location.reload();
+        //     } else if (ret.status == 'ENCODING' || ret.status == 'STARTED') {
+        //         $preloader.style.visibility = 'visible';
+        //         $trans.innerHTML = 'Encoding, please wait...';
+        //         setTimeout(update, 2000);
+        //     } else {
+        //         console.log("unknown status", ret);
+        //         $preloader.style.visibility = 'hidden';
+        //         $trans.innerHTML = ret.status + '...';
+        //         setTimeout(update, 5000);		
+        //     }
+        // });
+    }
+}
+
+var INLINE_JSON={
+    "transcript": "\n This is a sentence. And this is another sentence. And finally, we have a third sentence. Wow.\n",
+    "words": [
+        {
+            "alignedWord": "this",
+            "case": "success",
+            "end": 1.47,
+            "endOffset": 6,
+            "phones": [
+                {
+                    "duration": 0.11,
+                    "phone": "dh_B"
+                },
+                {
+                    "duration": 0.07,
+                    "phone": "ih_I"
+                },
+                {
+                    "duration": 0.27,
+                    "phone": "s_E"
+                }
+            ],
+            "start": 1.02,
+            "startOffset": 2,
+            "word": "This"
+        },
+        {
+            "alignedWord": "is",
+            "case": "success",
+            "end": 1.72,
+            "endOffset": 9,
+            "phones": [
+                {
+                    "duration": 0.12,
+                    "phone": "ih_B"
+                },
+                {
+                    "duration": 0.08,
+                    "phone": "z_E"
+                }
+            ],
+            "start": 1.52,
+            "startOffset": 7,
+            "word": "is"
+        },
+        {
+            "alignedWord": "a",
+            "case": "success",
+            "end": 1.8,
+            "endOffset": 11,
+            "phones": [
+                {
+                    "duration": 0.08,
+                    "phone": "ah_S"
+                }
+            ],
+            "start": 1.72,
+            "startOffset": 10,
+            "word": "a"
+        },
+        {
+            "alignedWord": "sentence",
+            "case": "success",
+            "end": 2.6100000000000003,
+            "endOffset": 20,
+            "phones": [
+                {
+                    "duration": 0.12,
+                    "phone": "s_B"
+                },
+                {
+                    "duration": 0.06,
+                    "phone": "eh_I"
+                },
+                {
+                    "duration": 0.05,
+                    "phone": "n_I"
+                },
+                {
+                    "duration": 0.06,
+                    "phone": "t_I"
+                },
+                {
+                    "duration": 0.07,
+                    "phone": "ah_I"
+                },
+                {
+                    "duration": 0.09,
+                    "phone": "n_I"
+                },
+                {
+                    "duration": 0.36,
+                    "phone": "s_E"
+                }
+            ],
+            "start": 1.8,
+            "startOffset": 12,
+            "word": "sentence"
+        },
+        {
+            "alignedWord": "and",
+            "case": "success",
+            "end": 4.51,
+            "endOffset": 25,
+            "phones": [
+                {
+                    "duration": 0.12,
+                    "phone": "ae_B"
+                },
+                {
+                    "duration": 0.1,
+                    "phone": "n_I"
+                },
+                {
+                    "duration": 0.11,
+                    "phone": "d_E"
+                }
+            ],
+            "start": 4.18,
+            "startOffset": 22,
+            "word": "And"
+        },
+        {
+            "alignedWord": "this",
+            "case": "success",
+            "end": 5.06,
+            "endOffset": 30,
+            "phones": [
+                {
+                    "duration": 0.01,
+                    "phone": "dh_B"
+                },
+                {
+                    "duration": 0.18,
+                    "phone": "ih_I"
+                },
+                {
+                    "duration": 0.36,
+                    "phone": "s_E"
+                }
+            ],
+            "start": 4.51,
+            "startOffset": 26,
+            "word": "this"
+        },
+        {
+            "alignedWord": "is",
+            "case": "success",
+            "end": 6.5200000000000005,
+            "endOffset": 33,
+            "phones": [
+                {
+                    "duration": 0.28,
+                    "phone": "ih_B"
+                },
+                {
+                    "duration": 0.29,
+                    "phone": "z_E"
+                }
+            ],
+            "start": 5.95,
+            "startOffset": 31,
+            "word": "is"
+        },
+        {
+            "alignedWord": "another",
+            "case": "success",
+            "end": 7.73,
+            "endOffset": 41,
+            "phones": [
+                {
+                    "duration": 0.1,
+                    "phone": "ah_B"
+                },
+                {
+                    "duration": 0.19,
+                    "phone": "n_I"
+                },
+                {
+                    "duration": 0.07,
+                    "phone": "ah_I"
+                },
+                {
+                    "duration": 0.11,
+                    "phone": "dh_I"
+                },
+                {
+                    "duration": 0.11,
+                    "phone": "er_E"
+                }
+            ],
+            "start": 7.15,
+            "startOffset": 34,
+            "word": "another"
+        },
+        {
+            "alignedWord": "sentence",
+            "case": "success",
+            "end": 8.52,
+            "endOffset": 50,
+            "phones": [
+                {
+                    "duration": 0.14,
+                    "phone": "s_B"
+                },
+                {
+                    "duration": 0.06,
+                    "phone": "eh_I"
+                },
+                {
+                    "duration": 0.08,
+                    "phone": "n_I"
+                },
+                {
+                    "duration": 0.06,
+                    "phone": "t_I"
+                },
+                {
+                    "duration": 0.07,
+                    "phone": "ah_I"
+                },
+                {
+                    "duration": 0.1,
+                    "phone": "n_I"
+                },
+                {
+                    "duration": 0.27,
+                    "phone": "s_E"
+                }
+            ],
+            "start": 7.74,
+            "startOffset": 42,
+            "word": "sentence"
+        },
+        {
+            "alignedWord": "and",
+            "case": "success",
+            "end": 10.24,
+            "endOffset": 55,
+            "phones": [
+                {
+                    "duration": 0.07,
+                    "phone": "ae_B"
+                },
+                {
+                    "duration": 0.05,
+                    "phone": "n_I"
+                },
+                {
+                    "duration": 0.05,
+                    "phone": "d_E"
+                }
+            ],
+            "start": 10.07,
+            "startOffset": 52,
+            "word": "And"
+        },
+        {
+            "alignedWord": "finally",
+            "case": "success",
+            "end": 10.930000000000001,
+            "endOffset": 63,
+            "phones": [
+                {
+                    "duration": 0.12,
+                    "phone": "f_B"
+                },
+                {
+                    "duration": 0.09,
+                    "phone": "ay_I"
+                },
+                {
+                    "duration": 0.06,
+                    "phone": "n_I"
+                },
+                {
+                    "duration": 0.07,
+                    "phone": "ah_I"
+                },
+                {
+                    "duration": 0.07,
+                    "phone": "l_I"
+                },
+                {
+                    "duration": 0.22,
+                    "phone": "iy_E"
+                }
+            ],
+            "start": 10.3,
+            "startOffset": 56,
+            "word": "finally"
+        },
+        {
+            "alignedWord": "we",
+            "case": "success",
+            "end": 11.51,
+            "endOffset": 67,
+            "phones": [
+                {
+                    "duration": 0.09,
+                    "phone": "w_B"
+                },
+                {
+                    "duration": 0.1,
+                    "phone": "iy_E"
+                }
+            ],
+            "start": 11.32,
+            "startOffset": 65,
+            "word": "we"
+        },
+        {
+            "alignedWord": "have",
+            "case": "success",
+            "end": 11.939999,
+            "endOffset": 72,
+            "phones": [
+                {
+                    "duration": 0.09,
+                    "phone": "hh_B"
+                },
+                {
+                    "duration": 0.21,
+                    "phone": "ae_I"
+                },
+                {
+                    "duration": 0.13,
+                    "phone": "v_E"
+                }
+            ],
+            "start": 11.509999,
+            "startOffset": 68,
+            "word": "have"
+        },
+        {
+            "alignedWord": "a",
+            "case": "success",
+            "end": 12.7,
+            "endOffset": 74,
+            "phones": [
+                {
+                    "duration": 0.12,
+                    "phone": "ah_S"
+                }
+            ],
+            "start": 12.58,
+            "startOffset": 73,
+            "word": "a"
+        },
+        {
+            "alignedWord": "third",
+            "case": "success",
+            "end": 13.03,
+            "endOffset": 80,
+            "phones": [
+                {
+                    "duration": 0.12,
+                    "phone": "th_B"
+                },
+                {
+                    "duration": 0.13,
+                    "phone": "er_I"
+                },
+                {
+                    "duration": 0.08,
+                    "phone": "d_E"
+                }
+            ],
+            "start": 12.7,
+            "startOffset": 75,
+            "word": "third"
+        },
+        {
+            "alignedWord": "sentence",
+            "case": "success",
+            "end": 13.66,
+            "endOffset": 89,
+            "phones": [
+                {
+                    "duration": 0.09,
+                    "phone": "s_B"
+                },
+                {
+                    "duration": 0.06,
+                    "phone": "eh_I"
+                },
+                {
+                    "duration": 0.06,
+                    "phone": "n_I"
+                },
+                {
+                    "duration": 0.05,
+                    "phone": "t_I"
+                },
+                {
+                    "duration": 0.06,
+                    "phone": "ah_I"
+                },
+                {
+                    "duration": 0.07,
+                    "phone": "n_I"
+                },
+                {
+                    "duration": 0.24,
+                    "phone": "s_E"
+                }
+            ],
+            "start": 13.03,
+            "startOffset": 81,
+            "word": "sentence"
+        },
+        {
+            "alignedWord": "wow",
+            "case": "success",
+            "end": 15.870000000000001,
+            "endOffset": 94,
+            "phones": [
+                {
+                    "duration": 0.17,
+                    "phone": "w_B"
+                },
+                {
+                    "duration": 0.24,
+                    "phone": "aw_E"
+                }
+            ],
+            "start": 15.46,
+            "startOffset": 91,
+            "word": "Wow"
+        }
+    ]
+};
+
+// Wait until we have video length.
+if ($a.duration) {
+    update()
+} else {
+    const onLoadedMetadata = () => {
+        $a.removeEventListener("loadedmetadata, onLoad")
+        update()
+    }
+    $a.addEventListener("loadedmetadata", onLoadedMetadata)
+}
